4b83aa222eb5af44edf2fe34d4a8db85598d024c
sarza-exe <sarazynd@gmail.com>
2025-05-02 16:02:47 +0200

    Tasks 1 and 3

--- Parallel/Lab2/task3/ex3.adb
+++ Parallel/Lab2/task3/ex3.adb
@@ -5,11 +5,12 @@ with Random_Seeds; use Random_Seeds;
 with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Characters.Handling; use Ada.Characters.Handling;
 
-procedure Ex1 is
+procedure Ex3 is
 
 -- Travelers moving on the board
   Nr_Of_Travelers : constant Integer := 15;
   Nr_Of_Wild_Travelers : constant Integer := 15;
+  Nr_Of_Traps : constant Integer := 10;
 
   Min_Steps : constant Integer := 10;
   Max_Steps : constant Integer := 100;
@@ -22,13 +23,13 @@ procedure Ex1 is
   Board_Width : constant Integer := 15;
   Board_Height : constant Integer := 15;
 
--- Timing
+  -- Global
+  Finish : Boolean := False;
   Start_Time : Time := Clock;
+  Seeds : Seed_Array_Type(1 .. Nr_Of_Travelers + Nr_Of_Wild_Travelers + Nr_Of_Traps)
+    := Make_Seeds(Nr_Of_Travelers + Nr_Of_Wild_Travelers + Nr_Of_Traps);
 
--- Random seeds for the tasks' random number generators
-  Seeds : Seed_Array_Type(1 .. Nr_Of_Travelers + Nr_Of_Wild_Travelers) := Make_Seeds(Nr_Of_Travelers + Nr_Of_Wild_Travelers);
-
--- Positions on the board
+  -- Positions
   type Position_Type is record
     X : Integer range 0 .. Board_Width;
     Y : Integer range 0 .. Board_Height;
@@ -91,7 +92,7 @@ procedure Ex1 is
 
   procedure Print_Traces(Traces : Traces_Sequence_Type) is
   begin
-    for I in 0 .. Traces.Last loop -- [0,... , Traces.Last]
+    for I in 0 .. Traces.Last loop
       Print_Trace(Traces.Trace_Array(I));
     end loop;
   end Print_Traces;
@@ -104,20 +105,33 @@ procedure Ex1 is
   task body Printer is
   begin
     for I in 1 .. Nr_Of_Travelers + Nr_Of_Wild_Travelers loop
-      accept Report(Traces : Traces_Sequence_Type) do -- it's not in order. It just waits until every thread has requested print
+      accept Report(Traces : Traces_Sequence_Type) do
+        Print_Traces(Traces); -- it's not in order. It just waits until every thread has requested print
+      end Report;
+    end loop;
+
+    Finish := True;
+
+    for I in 1 .. Nr_Of_Traps loop -- we gather traps reports after others do the same
+      accept Report(Traces : Traces_Sequence_Type) do
         Print_Traces(Traces);
       end Report;
     end loop;
   end Printer;
 
--- Travelers
-  type Traveler_Variant is (Legal, Wild, None);
+  -- Travelers
+  type Traveler_Variant is (Legal, Wild, Trap, None);
 
   type Traveler_Type is record
     Id       : Integer;
     Symbol   : Character;
     Position : Position_Type;
   end record;
+
+  -- Task types
+  type Response_Type is (Success, Fail, Trap, Deadlock);
+
+  type General_Traveler_Task_Type;
 
 -- Task type is a type of task that must be declared
 -- Task would just start.
@@ -129,24 +143,32 @@ procedure Ex1 is
   task type Wild_Traveler_Task_Type is
     entry Init(Id : Integer; Seed : Integer; Symbol : Character);
     entry Start;
-    entry Relocate(New_Position : Position_Type);
+    entry Relocate(New_Position : Position_Type; New_Response : Response_Type);
   end Wild_Traveler_Task_Type;
 
+  task type Trap_Task_Type is
+    entry Init(Id : Integer; Seed : Integer);
+    entry Its_A_Trap(Id : Integer; New_Response : in out Response_Type);
+  end Trap_Task_Type;
+
   type General_Traveler_Task_Type (Variant : Traveler_Variant) is record
+    Traveler : aliased Traveler_Type;
     case Variant is
       when Legal =>
         Traveler_Task : Traveler_Task_Type;
       when Wild =>
         Wild_Traveler_Task : Wild_Traveler_Task_Type;
+      when Trap =>
+        Trap_Task : Trap_Task_Type;
       when None =>
         null;
     end case;
   end record;
 
   protected type Tile is
-    -- Entry will block until 'when' conditions are met
+   -- Entry will block until 'when' conditions are met
     entry Init(New_Position : Position_Type);
-    entry Enter(New_Traveler : access General_Traveler_Task_Type; Success : out Boolean);
+    entry Enter(Id : Integer; Response : in out Response_Type);
     entry Leave;
   private
     Inited : Boolean := False;
@@ -158,11 +180,11 @@ procedure Ex1 is
   Board : array (0 .. Board_Width - 1, 0 .. Board_Height - 1) of Tile;
   -- Travel_Tasks allow entries in Tile to be called on any type of traveler
   -- access T type is equivalent to pointers
-  Travel_Tasks : array (0 .. Nr_Of_Travelers + Nr_Of_Wild_Travelers - 1) of access General_Traveler_Task_Type;
+  Travel_Tasks : array (0 .. Nr_Of_Travelers + Nr_Of_Wild_Travelers + Nr_Of_Traps - 1) of access General_Traveler_Task_Type;
   -- for removing travelers from board and initiating empty tiles
-  Null_Task : access General_Traveler_Task_Type := new General_Traveler_Task_Type(Variant => None);
+  Null_Task : constant access General_Traveler_Task_Type := new General_Traveler_Task_Type(Variant => None);
   
--- Task bodies
+  -- Task bodies
   protected body Tile is
     entry Init(New_Position : Position_Type) when not Inited is
     begin
@@ -174,12 +196,15 @@ procedure Ex1 is
     -- locks for legal travelers
     -- returns bool for wild travelers | tile must be initialized and traveler is none or wild
     -- Enter changes tile's travelers variable but if that happens we also have to change pos of travelers
-    entry Enter(New_Traveler : access General_Traveler_Task_Type; Success : out Boolean) when Inited and Traveler.Variant /= Legal is
+    entry Enter(Id : Integer; Response : in out Response_Type) when Inited and Traveler.Variant /= Legal is
+      New_Traveler : access General_Traveler_Task_Type;
     begin
-      -- if no one here, assign traveler
+      New_Traveler := Travel_Tasks(Id);
+
+      -- if noone here, assign traveler
       if Traveler.Variant = None then
-        Traveler := New_Traveler;
-        Success := True;
+        Traveler := Travel_Tasks(Id);
+        Response := Success;
 
       -- if wild here, try to move him
       elsif Traveler.Variant = Wild and New_Traveler.Variant = Legal then
@@ -191,28 +216,33 @@ procedure Ex1 is
             Move_Direction(New_Position, N);
             select
               -- try to enter
-              Board(New_Position.X, New_Position.Y).Enter(Traveler, Success);
-              if Success then -- if not success keep trying
-                exit;
-              end if;
+              Board(New_Position.X, New_Position.Y).Enter(Traveler.Traveler.Id, Response);
             else -- blocks if legal there - cant move
-              Success := False;
+              Response := Fail;
             end select;
+            exit when Response /= Fail;
           end loop;
 
-          if Success then -- signal wild and assign new traveler here
-            Traveler.Wild_Traveler_Task.Relocate(New_Position);
+          if Response /= Fail then -- signal wild and assign new traveler here
+            if Response /= Trap then -- if trapped task might not exist
+              Traveler.Wild_Traveler_Task.Relocate(New_Position, Success);
+            end if;
             Traveler := New_Traveler;
+            Response := Success;
           end if;
         end;
 
+      -- if trap move logic to trap task
+      elsif Traveler.Variant = Trap then
+        Traveler.Trap_Task.Its_A_Trap(Id, Response);
+
       -- any other cases - refuse enter
       else
-        Success := False;
+        Response := Fail;
       end if;
     end Enter;
 
-    -- used by travelers to leave the tile and by wildings to die
+   -- used by travelers to leave the tile and by wildings to die
     entry Leave when Inited is
     begin
       Traveler := Null_Task;
@@ -221,7 +251,7 @@ procedure Ex1 is
 
   task body Traveler_Task_Type is
     G             : Generator;
-    Traveler      : Traveler_Type;
+    Traveler      : access Traveler_Type;
     Time_Stamp    : Duration;
     Nr_of_Steps   : Integer;
     Traces        : Traces_Sequence_Type;
@@ -245,32 +275,36 @@ procedure Ex1 is
     end Make_Step;
 
   New_Position  : Position_Type;
-  Success       : Boolean;
-  Deadlock      : Boolean;
+  Response      : Response_Type;
   begin
--- [accept Init do <code> end Init] synchronizes with main. 
--- So main stops until accept do... end block finished
+   -- [accept Init do <code> end Init] synchronizes with main. 
+   -- So main stops until accept do... end block finished
     accept Init(Id : Integer; Seed : Integer; Symbol : Character) do
       Reset(G, Seed);
       Nr_of_Steps := Min_Steps + Integer(Float(Max_Steps - Min_Steps) * Random(G));
+      Traveler := Travel_Tasks(Id).Traveler'Access;
       Traveler.Id := Id;
       Traveler.Symbol := Symbol;
 
       -- try to move in
-      Success := False;
-      while not Success loop
+      Response := Fail;
+      while Response = Fail loop
         Traveler.Position := ( -- random position
           X => Integer(Float'Floor(Float(Board_Width) * Random(G))),
           Y => Integer(Float'Floor(Float(Board_Height) * Random(G)))
         );
         select
-          Board(Traveler.Position.X, Traveler.Position.Y).Enter(Travel_Tasks(Traveler.Id), Success);
+          Board(Traveler.Position.X, Traveler.Position.Y).Enter(Traveler.Id, Response);
         else
           null;
         end select;
       end loop;
+
+      if Response = Trap then -- leave board
+        Traveler.Position := (Board_Width, Board_Height);
+      end if;
+
       Time_Stamp := To_Duration(Clock - Start_Time);
-      -- Store starting position
       Store_Trace;
     end Init;
 
@@ -282,37 +316,40 @@ procedure Ex1 is
 
 -- it first makes all the steps noting it in Store_Trace
 -- then sends traces to printer.report
-    Deadlock := False;
     for Step in 0 .. Nr_of_Steps loop
+      exit when Response = Trap or Response = Deadlock;
       delay Min_Delay + (Max_Delay - Min_Delay) * Duration(Random(G));
 
       -- try to move
-      Success := False;
-      Deadlock := False;
-      while not Success loop
+      Response := Fail;
+      while Response = Fail loop
         New_Position := Traveler.Position;
         Make_Step(New_Position);
         select
-          -- Enter is used on the tile traveler wants to move to. So if there is another traveler it blocks
-          Board(New_Position.X, New_Position.Y).Enter(Travel_Tasks(Traveler.Id), Success);
+          Board(New_Position.X, New_Position.Y).Enter(Traveler.Id, Response);
         or
           delay 6 * Max_Delay; -- deadlock
-          Deadlock := True;
-          exit;
+          Response := Deadlock;
         end select;
       end loop;
 
-      if Deadlock then
-        Traveler.Symbol := To_Lower(Traveler.Symbol);
-        Time_Stamp := To_Duration(Clock - Start_Time);
-        Store_Trace;
-        exit;
-      else
-        Board(Traveler.Position.X, Traveler.Position.Y).Leave;
-        Traveler.Position := New_Position;
-        Time_Stamp := To_Duration(Clock - Start_Time);
-        Store_Trace;
-      end if;
+      -- handle response
+      case Response is
+        when Success =>
+          Board(Traveler.Position.X, Traveler.Position.Y).Leave;
+          Traveler.Position := New_Position;
+        when Trap =>
+          Board(Traveler.Position.X, Traveler.Position.Y).Leave;
+          Traveler.Position := (Board_Width, Board_Height);
+        when Deadlock =>
+          Traveler.Symbol := To_Lower(Traveler.Symbol);
+        when others =>
+          null;
+      end case;
+
+      -- store trace
+      Time_Stamp := To_Duration(Clock - Start_Time);
+      Store_Trace;
     end loop;
 
     Printer.Report(Traces);
@@ -320,11 +357,11 @@ procedure Ex1 is
 
   task body Wild_Traveler_Task_Type is
     G              : Generator;
-    Traveler       : Traveler_Type;
-    Time_Stamp     : Duration;
-    Traces         : Traces_Sequence_Type;
+    Traveler       : access Traveler_Type;
     Time_Appear    : Duration;
     Time_Disappear : Duration;
+    Time_Stamp     : Duration;
+    Traces         : Traces_Sequence_Type;
 
     procedure Store_Trace is
     begin
@@ -337,15 +374,16 @@ procedure Ex1 is
       );
     end Store_Trace;
 
-  Success        : Boolean;
+  Response        : Response_Type;
   begin
     accept Init(Id : Integer; Seed : Integer; Symbol : Character) do
       Reset(G, Seed);
-      Traveler.Id := Id;
-      Traveler.Symbol := Symbol;
       -- Time_Appear is anything in range [0, Max_Delay*Max_Steps)
       Time_Appear := (Max_Delay * Max_Steps) * Duration(Random(G));
       Time_Disappear := Time_Appear + (Max_Delay * Max_Steps - Time_Appear) * Duration(Random(G));
+      Traveler := Travel_Tasks(Id).Traveler'Access;
+      Traveler.Id := Id;
+      Traveler.Symbol := Symbol;
     end Init;
 
     accept Start do
@@ -353,44 +391,135 @@ procedure Ex1 is
     end Start;
 
     delay Time_Appear;
-
-    Success := False;
-    while not Success loop
+    
+    Response := Fail;
+    while Response = Fail loop
       Traveler.Position := ( -- random initial position
         X => Integer(Float'Floor(Float(Board_Width) * Random(G))),
         Y => Integer(Float'Floor(Float(Board_Height) * Random(G)))
       );
       select
-        Board(Traveler.Position.X, Traveler.Position.Y).Enter(Travel_Tasks(Traveler.Id), Success);
+        Board(Traveler.Position.X, Traveler.Position.Y).Enter(Traveler.Id, Response);
       else
         null;
       end select;
     end loop;
+
     Time_Stamp := To_Duration(Clock - Start_Time);
     Store_Trace;
 
-    loop
-      select
-        -- forceful relocation
-        accept Relocate(New_Position : Position_Type) do
+    loop -- main loop
+      exit when Response = Trap or To_Duration(Clock - Start_Time) >= Time_Disappear;
+
+      select -- forceful relocation
+        accept Relocate(New_Position : Position_Type; New_Response : Response_Type) do
+          Response := New_Response;
           Traveler.Position := New_Position;
           Time_Stamp := To_Duration(Clock - Start_Time);
           Store_Trace;
         end Relocate;
       or
         delay 0.1; -- keep checking time
-        if To_Duration(Clock - Start_Time) >= Time_Disappear then
-          Board(Traveler.Position.X, Traveler.Position.Y).Leave;
-          Traveler.Position := (X => Board_Width, Y => Board_Height);
+      end select;
+    end loop;
+
+    -- free the board
+    if Response /= Trap then
+      Board(Traveler.Position.X, Traveler.Position.Y).Leave;
+      Traveler.Position := (Board_Width, Board_Height);
+      Time_Stamp := To_Duration(Clock - Start_Time);
+      Store_Trace;
+    end if;
+
+    Printer.Report(Traces);
+  end Wild_Traveler_Task_Type;
+
+  task body Trap_Task_Type is
+    G             : Generator;
+    Trap_Traveler : access Traveler_Type;
+    Time_Stamp    : Duration;
+    Traces        : Traces_Sequence_Type;
+
+    procedure Store_Trace is
+    begin
+      Traces.Last := Traces.Last + 1;
+      Traces.Trace_Array(Traces.Last) := (
+        Time_Stamp => Time_Stamp,
+        Id         => Trap_Traveler.Id,
+        Position   => Trap_Traveler.Position,
+        Symbol     => Trap_Traveler.Symbol
+      );
+    end Store_Trace;
+
+  Response        : Response_Type;
+  Traveler        : access General_Traveler_Task_Type := Null_Task;
+  begin
+    accept Init(Id : Integer; Seed : Integer) do
+      Reset(G, Seed);
+      Trap_Traveler := Travel_Tasks(Id).Traveler'Access;
+      Trap_Traveler.Id := Id;
+      Trap_Traveler.Symbol := '#';
+
+      -- try to move in
+      Response := Fail;
+      while Response = Fail loop
+        Trap_Traveler.Position := ( -- random position
+          X => Integer(Float'Floor(Float(Board_Width) * Random(G))),
+          Y => Integer(Float'Floor(Float(Board_Height) * Random(G)))
+        );
+        select
+          Board(Trap_Traveler.Position.X, Trap_Traveler.Position.Y).Enter(Trap_Traveler.Id, Response);
+        else
+          null;
+        end select;
+      end loop;
+
+      Time_Stamp := To_Duration(Clock - Start_Time);
+      Store_Trace;
+    end Init;
+
+    loop -- main loop
+      exit when Finish;
+
+      select -- catch traveler
+        accept Its_A_Trap(Id : Integer; New_Response : in out Response_Type) do
+          Traveler := Travel_Tasks(Id);
+          case Traveler.Variant is
+            when Legal =>
+              Response := Trap;
+              Trap_Traveler.Symbol := To_Lower(Traveler.Traveler.Symbol);
+            when Wild =>
+              select -- nudge Wild
+                Traveler.Wild_Traveler_Task.Relocate((Board_Width, Board_Height), Trap);
+                Response := Trap;
+                Trap_Traveler.Symbol := '*';
+              else
+                Response := Fail;
+              end select;
+            when others =>
+              Response := Fail;
+          end case;
+          New_Response := Response;
+        end Its_A_Trap;
+
+        -- if traveler caught
+        if Response = Trap then
           Time_Stamp := To_Duration(Clock - Start_Time);
           Store_Trace;
-          exit;
+          -- block
+          delay 2 * Max_Delay; -- example amount of time
+          -- go back
+          Trap_Traveler.Symbol := '#';
+          Time_Stamp := To_Duration(Clock - Start_Time);
+          Store_Trace;
         end if;
+      or
+        delay 0.1; -- keep chacking time
       end select;
     end loop;
 
     Printer.Report(Traces);
-  end Wild_Traveler_Task_Type;
+  end Trap_Task_Type;
 
 
 -- Local for main task
@@ -399,23 +528,30 @@ procedure Ex1 is
   Id : Integer;
 begin -- beginning of MAIN
 
-  -- Print the line with the parameters needed for display script:
+-- Print the line with the parameters needed for display script:
   Put_Line(
     "-1 " &
-    Integer'Image(Nr_Of_Travelers + Nr_Of_Wild_Travelers) & " " &
+    Integer'Image(Nr_Of_Travelers + Nr_Of_Wild_Travelers + Nr_Of_Traps) & " " &
     Integer'Image(Board_Width) & " " &
     Integer'Image(Board_Height)
   );
 
-  -- Initialize board's tiles
+-- Initialize board's tiles
   for I in 0 .. Board_Width - 1 loop
     for J in 0 .. Board_Height - 1 loop
       Board(I, J).Init((X => I, Y => J));
     end loop;
   end loop;
 
--- init travelers tasks
+-- init trap tasks
   Id := 0;
+  for I in 0 .. Nr_Of_Traps - 1 loop
+    Travel_Tasks(Id) := new General_Traveler_Task_Type(Variant => Trap);
+    Travel_Tasks(Id).Trap_Task.Init(Id, Seeds(Id + 1));
+    Id := Id + 1;
+  end loop;
+
+-- init travelers tasks
   Symbol := 'A';
   for I in 0 .. Nr_Of_Travelers - 1 loop
     Travel_Tasks(Id) := new General_Traveler_Task_Type(Variant => Legal);
@@ -438,17 +574,17 @@ begin -- beginning of MAIN
     Id := Id + 1;
   end loop;
 
-   -- start tarvelers tasks
-  Id := 0;
+-- start tarvelers tasks
+  Id := Nr_Of_Traps;
   for I in 0 .. Nr_Of_Travelers - 1 loop
     Travel_Tasks(Id).Traveler_Task.Start;
     Id := Id + 1;
   end loop;
 
-   -- start wild tarvelers tasks
+-- start wild tarvelers tasks
   for I in 0 .. Nr_Of_Wild_Travelers - 1 loop
     Travel_Tasks(Id).Wild_Traveler_Task.Start;
     Id := Id + 1;
   end loop;
 
-end Ex1;
+end Ex3;
