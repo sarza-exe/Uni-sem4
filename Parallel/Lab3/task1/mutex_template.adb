fd66a804a5a1d4f870ecee6b61eaa88ad2a0dd05
sarza-exe <sarazynd@gmail.com>
2025-05-23 22:57:41 +0200

    Task 1 done. Started working on task 2

--- Parallel/Lab3/task1/mutex_template.adb
+++ Parallel/Lab3/task1/mutex_template.adb
@@ -1,236 +1,291 @@
-with Ada.Text_IO;          use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
-with Random_Seeds;           use Random_Seeds;
-with Ada.Real_Time;          use Ada.Real_Time;
+with Random_Seeds; use Random_Seeds;
+with Ada.Real_Time; use Ada.Real_Time;
 
-procedure Mutex_Template is
+procedure  Mutex_Template is
 
-   -- Configuration constants
-   Nr_Of_Processes : constant Integer := 15;
-   Min_Steps       : constant Integer := 50;
-   Max_Steps       : constant Integer := 100;
-   Min_Delay       : constant Duration := 0.01;
-   Max_Delay       : constant Duration := 0.05;
 
-   -- Process states
-   type Process_State is (Local_Section, Entry_Protocol, Critical_Section, Exit_Protocol);
+-- Processes 
 
-   Board_Width  : constant Integer := Nr_Of_Processes;
-   Board_Height : constant Integer := Process_State'Pos(Process_State'Last) + 1;
+  Nr_Of_Processes : constant Integer :=15;
 
-   Start_Time : Time := Clock;
+  Min_Steps : constant Integer := 50 ;
+  Max_Steps : constant Integer := 100 ;
 
-   Seeds : Seed_Array_Type(1 .. Nr_Of_Processes) := Make_Seeds(Nr_Of_Processes);
+  Min_Delay : constant Duration := 0.01;
+  Max_Delay : constant Duration := 0.05;
 
-   -- Position and trace types
-   type Position_Type is record
-      X : Integer range 0 .. Board_Width - 1;
-      Y : Integer range 0 .. Board_Height - 1;
-   end record;
+-- States of a Process 
 
-   type Trace_Type is record
-      Time_Stamp : Duration;
-      Id         : Integer;
-      Position   : Position_Type;
-      Symbol     : Character;
-   end record;
+  type Process_State is (
+    Local_Section,
+    Entry_Protocol,
+    Critical_Section,
+    Exit_Protocol
+    );
 
-   type Trace_Array_Type is array (0 .. Max_Steps) of Trace_Type;
+-- 2D Board display board
 
-   type Traces_Sequence_Type is record
-      Last        : Integer := -1;
-      Trace_Array : Trace_Array_Type;
-   end record;
+  Board_Width  : constant Integer := Nr_Of_Processes;
+  Board_Height : constant Integer := Process_State'Pos( Process_State'Last ) + 1;
 
-   type Boolean_Array is array(0 .. Nr_Of_Processes - 1) of Boolean;
-   type Integer_Array is array(0 .. Nr_Of_Processes - 1) of Integer;
+-- Timing
 
-   protected Bakery_State is
-      entry Set_Choosing(Id : Integer; Value : Boolean);
-      entry Set_Number(Id : Integer; Ticket : Integer);
-      function Max_Number return Integer;
-      function Choosing(Id : Integer) return Boolean;
-      function Ticket(Id : Integer) return Integer;
-      function Global_Max return Integer;
-   private
-      Choosing_Array : Boolean_Array := (others => False);
-      Ticket_Array   : Integer_Array := (others => 0);
-      Max_Ticket     : Integer := 0;
-   end Bakery_State;
+  Start_Time : Time := Clock;  -- global startnig time
 
-   protected body Bakery_State is
-      entry Set_Choosing(Id : Integer; Value : Boolean) when True is
-      begin
-         Choosing_Array(Id) := Value;
-      end Set_Choosing;
+-- Random seeds for the tasks' random number generators
+ 
+  Seeds : Seed_Array_Type( 1..Nr_Of_Processes ) := Make_Seeds( Nr_Of_Processes );
 
-      entry Set_Number(Id : Integer; Ticket : Integer) when True is
-      begin
-         Ticket_Array(Id) := Ticket;
-         if Ticket > Max_Ticket then
-            Max_Ticket := Ticket;
-         end if;
-      end Set_Number;
+-- Types, procedures and functions
 
-      function Max_Number return Integer is
-         Max_Val : Integer := 0;
-      begin
-         for J in Ticket_Array'Range loop
-            if Ticket_Array(J) > Max_Val then
-               Max_Val := Ticket_Array(J);
-            end if;
-         end loop;
-         return Max_Val;
-      end Max_Number;
+  -- Postitions on the board
+  type Position_Type is record	
+    X: Integer range 0 .. Board_Width - 1; 
+    Y: Integer range 0 .. Board_Height - 1; 
+  end record;	   
 
-      function Choosing(Id : Integer) return Boolean is
-      begin
-         return Choosing_Array(Id);
-      end Choosing;
+  -- Max Ticket
 
-      function Ticket(Id : Integer) return Integer is
-      begin
-         return Ticket_Array(Id);
-      end Ticket;
+  protected type Max_Ticket_Type is
+    procedure Set(New_Ticket : in Integer);
+    function Get return Integer;
+  private
+    Ticket : Integer := 0;
+  end Max_Ticket_Type;
 
-      function Global_Max return Integer is
-      begin
-         return Max_Ticket;
-      end Global_Max;
-   end Bakery_State;
-
-   -- Printer task for display
-   task Printer is
-      entry Report(Traces : Traces_Sequence_Type);
-   end Printer;
-
-   task body Printer is
+  protected body Max_Ticket_Type is
+   procedure Set(New_Ticket : in Integer) is
    begin
-      for I in 1 .. Nr_Of_Processes loop
-         accept Report(Traces : Traces_Sequence_Type) do
-            null;
-         end Report;
-         -- Print individual traces
-         for J in 0 .. Traces.Last loop
-            Put_Line(Duration'Image(Traces.Trace_Array(J).Time_Stamp)
-                     & " " & Integer'Image(Traces.Trace_Array(J).Id)
-                     & " " & Integer'Image(Traces.Trace_Array(J).Position.X)
-                     & " " & Integer'Image(Traces.Trace_Array(J).Position.Y)
-                     & " " & Traces.Trace_Array(J).Symbol);
-         end loop;
+      if New_Ticket > Ticket then
+        Ticket := New_Ticket;
+      end if;
+   end Set;
+
+   function Get return Integer is
+   begin
+      return Ticket;
+   end Get;
+  end Max_Ticket_Type;
+
+  Max_Ticket : Max_Ticket_Type;
+
+  -- traces of Processes
+  type Trace_Type is record 	      
+    Time_Stamp:  Duration;	      
+    Id : Integer;
+    Position: Position_Type;      
+    Symbol: Character;	      
+  end record;	      
+
+  type Trace_Array_type is  array(0 .. Max_Steps) of Trace_Type;
+
+  type Traces_Sequence_Type is record
+    Last: Integer := -1;
+    Trace_Array: Trace_Array_type ;
+  end record; 
+
+
+  procedure Print_Trace( Trace : Trace_Type ) is
+    Symbol : String := ( ' ', Trace.Symbol );
+  begin
+    Put_Line(
+        Duration'Image( Trace.Time_Stamp ) & " " &
+        Integer'Image( Trace.Id ) & " " &
+        Integer'Image( Trace.Position.X ) & " " &
+        Integer'Image( Trace.Position.Y ) & " " &
+        ( ' ', Trace.Symbol ) -- print as string to avoid: '
+      );
+  end Print_Trace;
+
+  procedure Print_Traces( Traces : Traces_Sequence_Type ) is
+  begin
+    for I in 0 .. Traces.Last loop
+      Print_Trace( Traces.Trace_Array( I ) );
+    end loop;
+  end Print_Traces;
+
+
+  -- task Printer collects and prints reports of traces and the line with the parameters
+
+  task Printer is
+    entry Report( Traces : Traces_Sequence_Type );
+  end Printer;
+  
+  task body Printer is 
+  begin
+  
+    -- Collect and print the traces
+    
+    for I in 1 .. Nr_Of_Processes loop -- range for TESTS !!!
+        accept Report( Traces : Traces_Sequence_Type ) do
+          -- Put_Line("I = " & I'Image );
+          Print_Traces( Traces );
+        end Report;
       end loop;
 
-      -- Print display parameters line
-      Put("-1 " & Integer'Image(Nr_Of_Processes) & " "
-          & Integer'Image(Board_Width) & " " & Integer'Image(Board_Height) & " ");
-      for S in Process_State'Range loop
-         Put(S'Image & ";");
+    -- Prit the line with the parameters needed for display script:
+
+    Put(
+      "-1 "&
+      Integer'Image( Nr_Of_Processes ) &" "&
+      Integer'Image( Board_Width ) &" "&
+      Integer'Image( Board_Height ) &" "       
+    );
+    for I in Process_State'Range loop
+      Put( I'Image &";" );
+    end loop;
+    -- Place labels with extra info here (e.g. "MAX_TICKET=...;" for Backery). 
+    -- 'Image produces space in front of a number. This removes it
+    declare
+      S : constant String := Integer'Image(Max_Ticket.Get);
+    begin
+      Put_Line("MAX_TICKET=" & S(S'First+1 .. S'Last) & ";");
+    end;
+
+  end Printer;
+
+   --pragma Time_Slice(0.01);
+
+   Choosing: array (0 .. Nr_Of_Processes-1) of Integer range 0 .. 1 := (others => 0);
+   Number: array (0 .. Nr_Of_Processes-1) of Integer := (others => 0);
+
+   --pragma Volatile(Choosing);
+   --pragma Volatile(Number);
+
+  -- Processes
+  type Process_Type is record
+    Id: Integer;
+    Symbol: Character;
+    Position: Position_Type;    
+  end record;
+
+
+  task type Process_Task_Type is	
+    entry Init(Id: Integer; Seed: Integer; Symbol: Character);
+    entry Start;
+  end Process_Task_Type;	
+
+  task body Process_Task_Type is
+    G : Generator;
+    Process : Process_Type;
+    Time_Stamp : Duration;
+    Nr_of_Steps: Integer;
+    Traces: Traces_Sequence_Type; 
+    Local_Max_Ticket : Integer := 0;
+
+    -- Max Ticket currently waiting
+    function Max return Integer is
+      Current : Integer := 0;
+    begin
+      for N in Number'Range loop
+        if Number(N) > Current then
+          Current := Number(N);
+        end if;
       end loop;
-      Put_Line("MAX_TICKET=" & Integer'Image(Bakery_State.Global_Max) & ";");
-   end Printer;
+      return Current;
+    end Max;
 
-   -- Process task type
-   task type Process_Task_Type is
-      entry Init(Id : Integer; Seed : Integer; Symbol : Character);
-      entry Start;
-   end Process_Task_Type;
+    procedure Store_Trace is
+    begin  
+      Traces.Last := Traces.Last + 1;
+      Traces.Trace_Array( Traces.Last ) := ( 
+          Time_Stamp => Time_Stamp,
+          Id => Process.Id,
+          Position => Process.Position,
+          Symbol => Process.Symbol
+        );
+    end Store_Trace;
 
-   type Process is record
-      Id       : Integer;
-      Symbol   : Character;
-      Position : Position_Type;
-   end record;
+    procedure Change_State( State: Process_State ) is
+    begin
+      Time_Stamp := To_Duration ( Clock - Start_Time ); -- reads global clock
+      Process.Position.Y := Process_State'Pos( State );
+      Store_Trace;
+    end;
+    
 
-   task body Process_Task_Type is
-      G          : Generator;
-      Time_Stamp : Duration;
-      Nr_of_Steps: Integer;
-      Traces     : Traces_Sequence_Type;
+  begin
+    accept Init(Id: Integer; Seed: Integer; Symbol: Character) do
+      Reset(G, Seed); 
+      Process.Id := Id;
+      Process.Symbol := Symbol;
+      -- Initial position 
+      Process.Position := (
+          X => Id,
+          Y => Process_State'Pos( LOCAL_SECTION )
+        );
+      -- Number of steps to be made by the Process  
+      Nr_of_Steps := Min_Steps + Integer( Float(Max_Steps - Min_Steps) * Random(G));
+      -- Time_Stamp of initialization
+      Time_Stamp := To_Duration ( Clock - Start_Time ); -- reads global clock
+      Store_Trace; -- store starting position
+    end Init;
+    
+    -- wait for initialisations of the remaining tasks:
+    accept Start do
+      null;
+    end Start;
 
-      procedure Store_Trace is
-      begin
-         Traces.Last := Traces.Last + 1;
-         Traces.Trace_Array(Traces.Last) := (
-            Time_Stamp => Time_Stamp,
-            Id         => Process.Id,
-            Position   => Process.Position,
-            Symbol     => Process.Symbol);
-      end Store_Trace;
+--    for Step in 0 .. Nr_of_Steps loop
+    for Step in 0 .. Nr_of_Steps/4 - 1  loop  -- TEST !!!
+      -- LOCAL_SECTION - start
+      delay Min_Delay+(Max_Delay-Min_Delay)*Duration(Random(G));
+      -- LOCAL_SECTION - end
 
-      procedure Change_State(State : Process_State) is
-      begin
-         Time_Stamp := To_Duration(Clock - Start_Time);
-         Process.Position.Y := Process_State'Pos(State);
-         Store_Trace;
-      end Change_State;
+      Change_State( ENTRY_PROTOCOL ); -- starting ENTRY_PROTOCOL
 
-   begin  -- Process body
-      accept Init(Id : Integer; Seed : Integer; Symbol : Character) do
-         Reset(G, Seed);
-         Process.Id     := Id;
-         Process.Symbol := Symbol;
-         Process.Position := (X => Id, Y => Process_State'Pos(Local_Section));
-         Nr_of_Steps := Min_Steps + Integer(Float(Max_Steps - Min_Steps) * Random(G));
-         Time_Stamp  := To_Duration(Clock - Start_Time);
-         Store_Trace;
-      end Init;
-
-      accept Start;
-
-      for Step in 0 .. Nr_of_Steps-1 loop
-         -- Local Section
-         delay Min_Delay + (Max_Delay - Min_Delay) * Duration(Random(G));
-         Change_State(Entry_Protocol);
-
-         -- Bakery Entry Protocol
-         Bakery_State.Set_Choosing(Process.Id, True);
-         declare
-            My_Ticket : constant Integer := Bakery_State.Max_Number + 1;
-         begin
-            Bakery_State.Set_Ticket(Process.Id, My_Ticket);
-         end;
-         Bakery_State.Set_Choosing(Process.Id, False);
-
-         -- Wait for turn
-         for J in 0 .. Nr_Of_Processes-1 loop
-            if J /= Process.Id then
-               -- wait until J has chosen
-               while Bakery_State.Choosing(J) loop
-                  null;
-               end loop;
-               -- wait if J has lower ticket or same ticket and lower Id
-               while Bakery_State.Ticket(J) /= 0 and then
-                     (Bakery_State.Ticket(J) < Bakery_State.Ticket(Process.Id) or
-                      (Bakery_State.Ticket(J) = Bakery_State.Ticket(Process.Id) and then J < Process.Id)) loop
-                  null;
-               end loop;
-            end if;
-         end loop;
-
-         Change_State(Critical_Section);
-
-         -- Critical Section
-         delay Min_Delay + (Max_Delay - Min_Delay) * Duration(Random(G));
-
-         Change_State(Exit_Protocol);
-         -- Exit Protocol
-         Bakery_State.Set_Ticket(Process.Id, 0);
-
-         Change_State(Local_Section);
+      -- choosing is set to 1 while we choose our number
+      Choosing(Process.Id) := 1; 
+      Number(Process.Id) := 1 + Max;
+      Choosing(Process.Id) := 0;
+      if Number(Process.Id) > Local_Max_Ticket then
+        Local_Max_Ticket := Number(Process.Id);
+      end if;
+      for J in 0 .. Nr_Of_Processes-1 loop
+        if J /= Process.Id then
+          loop exit when Choosing(J) = 0; end loop;
+          loop exit when
+            (Number(J) = 0) or
+            (Number(Process.Id) < Number(J)) or
+            (Number(Process.Id) = Number(J) and Process.Id < J);
+          end loop;
+        end if;
       end loop;
 
-      Printer.Report(Traces);
-   end Process_Task_Type;
+      Change_State( CRITICAL_SECTION ); -- starting CRITICAL_SECTION
 
-   -- Main execution
-   Process_Tasks : array(0 .. Nr_Of_Processes-1) of Process_Task_Type;
-   Symbol        : Character := 'A';
-begin
-   for I in Process_Tasks'Range loop
-      Process_Tasks(I).Init(I, Seeds(I+1), Symbol);
-      Symbol := Character'Succ(Symbol);
-   end loop;
-   for I in Process_Tasks'Range loop
-      Process_Tasks(I).Start;
-   end loop;
+      -- CRITICAL_SECTION - start
+      delay Min_Delay+(Max_Delay-Min_Delay)*Duration(Random(G));
+      -- CRITICAL_SECTION - end
+
+      Change_State( EXIT_PROTOCOL ); -- starting EXIT_PROTOCOL
+      Number(Process.Id) := 0;
+      Change_State( LOCAL_SECTION ); -- starting LOCAL_SECTION      
+    end loop;
+    
+    Max_Ticket.Set(Local_Max_Ticket);
+    Printer.Report( Traces );
+  end Process_Task_Type;
+
+
+-- local for main task
+
+  Process_Tasks: array (0 .. Nr_Of_Processes-1) of Process_Task_Type; -- for tests
+  Symbol : Character := 'A';
+
+begin 
+  -- init tarvelers tasks
+  for I in Process_Tasks'Range loop
+    Process_Tasks(I).Init( I, Seeds(I+1), Symbol );   -- `Seeds(I+1)` is ugly :-(
+    Symbol := Character'Succ( Symbol );
+  end loop;
+
+  -- start tarvelers tasks
+  for I in Process_Tasks'Range loop
+    Process_Tasks(I).Start;
+  end loop;
+
 end Mutex_Template;
+
